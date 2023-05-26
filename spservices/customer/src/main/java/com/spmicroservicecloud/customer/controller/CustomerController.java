98c9ff665b51c20e4de1957bd6646d4b475fb745
Güven Karabulut <guven.07.568@gmail.com>
2023-05-26 18:23:26 +0300

    add zipkin config and logging

--- spservices/customer/src/main/java/com/spmicroservicecloud/customer/controller/CustomerController.java
+++ spservices/customer/src/main/java/com/spmicroservicecloud/customer/controller/CustomerController.java
@@ -1,10 +1,14 @@
 package com.spmicroservicecloud.customer.controller;
 
+import brave.sampler.Sampler;
 import com.spmicroservicecloud.customer.request.CustomerRegistrationRequest;
 import com.spmicroservicecloud.customer.service.CusttomerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/customers")
 @RequiredArgsConstructor
+@EnableAutoConfiguration
+@Slf4j
 public class CustomerController {
     private final CusttomerService customerService;
-    private static final Logger log = LoggerFactory.getLogger(CustomerController.class.getName());
-
+    @Bean
+    public Sampler alwaysSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
         log.info("new customer registration {}", customerRegistrationRequest);
--- spservices/fraud/src/main/java/com/spmicroservicecloud/fraud/controller/FraudCheckHistoryController.java
+++ spservices/fraud/src/main/java/com/spmicroservicecloud/fraud/controller/FraudCheckHistoryController.java
@@ -1,9 +1,11 @@
 package com.spmicroservicecloud.fraud.controller;
 
+import brave.sampler.Sampler;
 import com.spmicroservicecloud.fraud.response.FraudCheckResponse;
 import com.spmicroservicecloud.fraud.service.FraudCheckHistoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class FraudCheckHistoryController {
     private final FraudCheckHistoryService fraudCheckHistoryService;
+    @Bean
+    public Sampler alwaysSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
     @GetMapping ("/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable Integer customerId){
         boolean isFraudulentCustomer= fraudCheckHistoryService.isFraudulentCustomer(customerId);
